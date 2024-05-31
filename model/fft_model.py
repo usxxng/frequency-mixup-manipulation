b0486b8c2ae494b90fb0cc8f6ff96752c44725b1
useung <you-seung1227@daum.net>
2024-05-31 16:28:58 +0900

    Add files via upload

---
+++ model/fft_model.py
@@ -0,0 +1,117 @@
+import torch, torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=1):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        b, c, _, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1, 1)
+        return x * y.expand_as(x)
+
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True,
+                 bn=True, bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm3d(out_planes, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class SpatialPool(nn.Module):
+    def forward(self, x):
+        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        kernel_size = 3
+        self.compress = SpatialPool()
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False)
+
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = torch.sigmoid(x_out)
+        self.attention = scale
+        return x * self.attention
+
+
+class Net(nn.Module):
+    def __init__(self, dropout=0):
+        nn.Module.__init__(self)
+        self.SpatialGate = SpatialGate()
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv3d(1, 8, 3),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            self.SpatialGate,
+            nn.ReLU(),
+            nn.Conv3d(8, 8, 3),
+            nn.BatchNorm3d(8),
+            nn.MaxPool3d(2, stride=2),
+            nn.ReLU(),
+            nn.Conv3d(8, 16, 3),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            nn.Conv3d(16, 16, 3),
+            nn.BatchNorm3d(16),
+            nn.MaxPool3d(2, stride=2),
+            nn.ReLU(),
+            nn.Conv3d(16, 32, 3),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            nn.Conv3d(32, 32, 3),
+            nn.BatchNorm3d(32),
+            nn.MaxPool3d(2, stride=2),
+            nn.ReLU(),
+            nn.Conv3d(32, 64, 3),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            nn.Conv3d(64, 64, 3),
+            nn.BatchNorm3d(64),
+            nn.MaxPool3d(2, stride=2),
+            nn.ReLU(),
+            nn.Conv3d(64, 128, 3),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            nn.Conv3d(128, 128, 3),
+            nn.BatchNorm3d(128),
+            nn.MaxPool3d(2, stride=2),
+            nn.ReLU(),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(128*2*3*2, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
+        )
+
+    def forward(self, x):
+        features = self.feature_extractor(x)
+        features = features.view(x.shape[0], -1)
+        logits = self.classifier(features)
+        return logits
